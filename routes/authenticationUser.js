bdbe0ddd4cb6b4af0f9543fbcb0e5de313df94c5
Zak Campuzano <zcamp22@gmail.com>
2018-11-27 12:10:50 -0500

    catalystDB setup

--- routes/authenticationUser.js
+++ routes/authenticationUser.js
@@ -11,6 +11,20 @@ module.exports = (router, session) => {
 
     mongoose.set('debug', true);
 
+    router.get('/', (req, res) => {
+        '<!DOCTYPE html>
+    <html lang="en">
+        <head>
+        <meta charset="UTF-8">
+        <title>Title</title>
+        </head>
+        <body>
+
+        </body>
+        </html>'
+    })
+
+
     /* ==============
       Create Token
    ============== */
