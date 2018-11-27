bcd69d830a6b0862dc3a691b643c6ce628bb120b
Zak Campuzano <zcamp22@gmail.com>
2018-11-27 12:25:02 -0500

    catalystDB setup

--- serverApp.js
+++ serverApp.js
@@ -32,7 +32,7 @@ app.use(methodOverride());
 app.use(cors());
 app.use('/authentication', authentication);
 app.use('/sportAuthentication', sportAuthentication);
-app.use('/', index);
+app.use('/', readfile('./index.html'));
 
 app.use(session({
     secret: config.secret,
